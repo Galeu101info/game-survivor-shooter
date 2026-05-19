83531febb73ce95356e47e6cd03f27495f85acaf
Galeu <guiwest2009@gmail.com>
2026-05-19 15:56:45 -0300

    Code slime to follow the player

---
+++ src/mob.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+var player 
+
+func _ready():
+	player = get_node("/root/Game/Playerq")
+
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300
+	move_and_slide()
